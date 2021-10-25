ed83dbc15b2dd399125471ef0e389f80a0386bed
Miroslav Slusarčík <miroslav@slusarcik.cz>
2021-10-25 16:33:29 +0200

    First look - web atelier

---
+++ client-doubrava/assets/js/main.js
@@ -0,0 +1,20 @@
+$(function() {
+  jQuery("body").addClass("jquery-loaded");
+  $('.basic-carousel').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true
+  });
+  $('.mobile-basic-carousel').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: "unslick"
+      }
+    ]
+  });
+});
