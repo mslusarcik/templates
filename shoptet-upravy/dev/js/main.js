648c1269ca7c317286ae6568ed3ecb9b3e0d01b8
Miroslav Slusarčík <miroslav@slusarcik.cz>
2022-03-11 23:15:22 +0100

    First commit

---
+++ shoptet-upravy/dev/js/main.js
@@ -0,0 +1,40 @@
+$(function() {
+  jQuery("body").addClass("jquery-loaded");
+  var swiper = new Swiper(".references-swiper", {
+    spaceBetween: 250,
+    effect: "coverflow",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 250,
+      modifier: 1,
+      slideShadows: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 5000,
+      pauseOnMouseEnter: true,
+    },
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+  });
+  const reviewsSwiper = new Swiper('.reviews-swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
