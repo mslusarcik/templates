034b02bdbc5a8b240cfdea7fac85ba46c97f9920
Miroslav Slusarčík <miroslav@slusarcik.cz>
2022-05-26 10:03:39 +0200

    5625

--- client-jares/dev/js/main.js
+++ client-jares/dev/js/main.js
@@ -1,3 +1,10 @@
 $(function() {
   jQuery("body").addClass("jquery-loaded");
+  $(".navbar-toggler-icon").click(function(){
+    $("body").toggleClass("menu-opened");
+  });
+  $(".js-scroll").click(function(e){
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    e.preventDefault();
+  });
 });
