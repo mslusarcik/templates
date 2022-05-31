fa3c40abba72681d9fc91b5920d6bc24b1124767
Miroslav Slusarčík <miroslav@slusarcik.cz>
2022-06-01 01:02:37 +0200

    Final update

--- client-jares/dev/js/main.js
+++ client-jares/dev/js/main.js
@@ -1,3 +1,27 @@
+function ajaxSendForm() {
+  $("form.ajax-form").submit(function (e) {
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+
+    var form = $(this);
+    var url = form.attr("action");
+
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: url,
+      data: form.serialize(), // serializes the form's elements.
+      success: function (data) {
+        jQuery("form.ajax-form").fadeOut(500);
+        jQuery(".form-success").css("display", "flex").hide().delay(600).fadeIn();
+      },
+      error: function () {
+        jQuery("form.ajax-form").fadeOut(500);
+        jQuery(".form-success").css("display", "flex").hide().delay(600).fadeIn();
+      }
+    });
+  });
+}
+
 $(function() {
   jQuery("body").addClass("jquery-loaded");
   $(".navbar-toggler-icon").click(function(){
@@ -7,4 +31,5 @@ $(function() {
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
     e.preventDefault();
   });
+  ajaxSendForm();
 });
